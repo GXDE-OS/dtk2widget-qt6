5587b6f1466e05e09ee85898a033f78fe0e4674e
MarcusPy827 <markus_verify@126.com>
2026-06-24 02:46:46 +0800

    fix: 「关于」对话框造成程序崩溃

--- src/widgets/ddialog.cpp
+++ src/widgets/ddialog.cpp
@@ -739,8 +739,66 @@ void DDialog::insertContent(int index, QWidget *widget, Qt::Alignment alignment)
 {
     D_D(DDialog);
 
-    d->contentLayout->insertWidget(index + DIALOG::CONTENT_INSERT_OFFSET,
-                                         widget, 0, alignment);
+    /** 
+     * 原来的值为2,本身在Qt5下是无害的，因为在Qt5上insertWidget越界会自动追加
+     * QBoxLayout::insertWidget在Qt6的行为是调用者给它的索引超过实际数量会损坏内部的QList结构
+     * 后续QLayout::activate()时直接段错误
+     * 也请参阅ddialog中对越界保护的说明
+     * 
+     * 索引0是内部的一个textLayout,应该是标题和消息用的
+     * 索引1开始才是用户通过addContent/insertContent添加的控件
+     * 我也不知道为什么原版会定义为2...
+     * ----------------------------------------------------------------------------
+     * The original value is 2, which is harmless in Qt5 because insertWidget will automatically
+     * append when index out of bound/range. However, in Qt6, QBoxLayout::instertWidget
+     * could DAMAGE its internal QList structure when index out of bound/range, causing SIGSEGV
+     * when QLayout::activate() is called later.
+     * Please also refer to the boundary check in ddialog.cpp.
+     *
+     * Index 0: Internal textLayout, should be used for title and message.
+     * Index 1 and above: User-added widgets by calling addContent/insertContent.
+     * I have absolutely no idea why the original code defines it as 2 either so don't ask me...
+     */
+
+     /**
+     * 真是怪了，放一个我当时用的调试指令和当时的输出在这里吧
+     * I felt wired either, so here is the debug command I used and its output at that time:
+     *
+     * LD_LIBRARY_PATH=~/Desktop/Repository/GXDE/UI/dtk2widget-qt6/build-qt6/src:$LD_LIBRARY_PATH \
+     * gdb -batch \
+     * -ex "set pagination off" \
+     * -ex "run" \
+     * -ex "bt full" \
+     * --args ~/Desktop/Repository/GXDE/UI/dtk2widget-qt6/build-qt6/examples/dwidget-examples/collections/collections
+     * ----------------------------------------------------------------------------
+     * Thread 1 "collections" received signal SIGSEGV, Segmentation fault.
+     *   0x00007ffff762ed15 in QBoxLayout::itemAt(int) const () from /lib/x86_64-linux-gnu/libQt6Widgets.so.6
+     *
+     *   #0   QBoxLayout::itemAt(int) const  # 这就是上面说的QBoxLayout This is the QBoxLayout mentioned above
+     *   #1   QLayout::activateRecursiveHelper(QLayoutItem*)  # 递归遍历QLayoutItem Recursively iterate those QLayoutItem
+     *   #2   QLayout::activateRecursiveHelper(QLayoutItem*)  # 递归 recursion
+     *   #3   QLayout::activateRecursiveHelper(QLayoutItem*)  # 递归 recursion
+     *   #4   QLayout::activate()  # 调用QLayout::activate() Called QLayout::activate()
+     *   #5   QWidgetPrivate::setVisible(bool)
+     *   #6   QDialogPrivate::setVisible(bool)
+     *   #7   QDialog::exec()
+     *   #8   Dtk::Widget::DDialog::exec()
+     *   #9   Dtk::Widget::DApplication::handleAboutAction()  #  打开「关于」对话框 Triggerred by opening the About dialog
+     * ----------------------------------------------------------------------------
+     * 段错误是第三次递归时触发的，内存访问非法，应该是野指针
+     * Note that the SIGSEGV is triggered at the third recursion, which is an illegal memory access
+     * and should be a wild pointer.
+     */
+
+    // 在这里做防御性编程：如果插入的index越界了直接给定为当前局部的count
+    // So here is my defense strategy here: if index out of bound/range, force set it
+    // to the current index count.
+    int targetIndex = index + DIALOG::CONTENT_INSERT_OFFSET;
+    if (targetIndex > d->contentLayout->count()) {
+        targetIndex = d->contentLayout->count();
+    }
+
+    d->contentLayout->insertWidget(targetIndex, widget, 0, alignment);
     d->contentList << widget;
 }
 
--- src/widgets/dialog_constants.h
+++ src/widgets/dialog_constants.h
@@ -22,12 +22,65 @@
 
 DWIDGET_BEGIN_NAMESPACE
 
+/** 
+ * CONTENT_INSERT_OFFSET定义值改动说明
+ * 原来的值为2,本身在Qt5下是无害的，因为在Qt5上insertWidget越界会自动追加
+ * QBoxLayout::insertWidget在Qt6的行为是调用者给它的索引超过实际数量会损坏内部的QList结构
+ * 后续QLayout::activate()时直接段错误
+ * 也请参阅ddialog中对越界保护的说明
+ * 
+ * 索引0是内部的一个textLayout,应该是标题和消息用的
+ * 索引1开始才是用户通过addContent/insertContent添加的控件
+ * 我也不知道为什么原版会定义为2...
+ * ----------------------------------------------------------------------------
+ * The change of CONTENT_INSERT_OFFSET value
+ * The original value is 2, which is harmless in Qt5 because insertWidget will automatically
+ * append when index out of bound/range. However, in Qt6, QBoxLayout::instertWidget
+ * could DAMAGE its internal QList structure when index out of bound/range, causing SIGSEGV
+ * when QLayout::activate() is called later.
+ * Please also refer to the boundary check in ddialog.cpp.
+ *
+ * Index 0: Internal textLayout, should be used for title and message.
+ * Index 1 and above: User-added widgets by calling addContent/insertContent.
+ * I have absolutely no idea why the original code defines it as 2 either so don't ask me...
+ */
+
+/**
+ * 真是怪了，放一个我当时用的调试指令和当时的输出在这里吧
+ * I felt wired either, so here is the debug command I used and its output at that time:
+ *
+ * LD_LIBRARY_PATH=~/Desktop/Repository/GXDE/UI/dtk2widget-qt6/build-qt6/src:$LD_LIBRARY_PATH \
+ * gdb -batch \
+ * -ex "set pagination off" \
+ * -ex "run" \
+ * -ex "bt full" \
+ * --args ~/Desktop/Repository/GXDE/UI/dtk2widget-qt6/build-qt6/examples/dwidget-examples/collections/collections
+ * ----------------------------------------------------------------------------
+ * Thread 1 "collections" received signal SIGSEGV, Segmentation fault.
+ *   0x00007ffff762ed15 in QBoxLayout::itemAt(int) const () from /lib/x86_64-linux-gnu/libQt6Widgets.so.6
+ *
+ *   #0   QBoxLayout::itemAt(int) const  # 这就是上面说的QBoxLayout This is the QBoxLayout mentioned above
+ *   #1   QLayout::activateRecursiveHelper(QLayoutItem*)  # 递归遍历QLayoutItem Recursively iterate those QLayoutItem
+ *   #2   QLayout::activateRecursiveHelper(QLayoutItem*)  # 递归 recursion
+ *   #3   QLayout::activateRecursiveHelper(QLayoutItem*)  # 递归 recursion
+ *   #4   QLayout::activate()  # 调用QLayout::activate() Called QLayout::activate()
+ *   #5   QWidgetPrivate::setVisible(bool)
+ *   #6   QDialogPrivate::setVisible(bool)
+ *   #7   QDialog::exec()
+ *   #8   Dtk::Widget::DDialog::exec()
+ *   #9   Dtk::Widget::DApplication::handleAboutAction()  #  打开「关于」对话框 Triggerred by opening the About dialog
+ * ----------------------------------------------------------------------------
+ * 段错误是第三次递归时触发的，内存访问非法，应该是野指针
+ * Note that the SIGSEGV is triggered at the third recursion, which is an illegal memory access
+ * and should be a wild pointer.
+ */
+
 namespace DIALOG {
     const int DEFAULT_WIDTH = 380;
     const int DEFAULT_HEIGHT = 120;
     const int BORDER_SHADOW_WIDTH = 0;
     const int BORDER_RADIUS = 4;
-    const int CONTENT_INSERT_OFFSET = 2;
+    const int CONTENT_INSERT_OFFSET = 1;
     const int BUTTON_HEIGHT = 28;
     const int CLOSE_BUTTON_WIDTH = 27;
     const int CLOSE_BUTTON_HEIGHT = 23;
